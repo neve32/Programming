1bcfc5caae6c136306733d6658c1e24de57658fe
Nevetha <31408489+neve32@users.noreply.github.com>
2019-08-07 23:12:05 +0530

    Update RemoveRepeatedLetter.cpp

--- RemoveRepeatedLetter.cpp
+++ RemoveRepeatedLetter.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+   string S,res="";
+   cin>>S;
+   int hash[1234]={0};
+   for(int i=0;i<S.length();i++)
+   {
+      if(hash[S[i]]==0)
+      {
+        hash[S[i]]=1;
+        res=res+S[i];
+      }
+   }
+  cout<<res;
+}
